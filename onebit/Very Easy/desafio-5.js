173ec8e27384f1eeb5045de658be4ffcfe771b7e
genildocs <genildocs@gmail.com>
2024-07-31 05:08:02 -0300

    commit

--- onebit/Very Easy/desafio-5.js
+++ onebit/Very Easy/desafio-5.js
@@ -11,3 +11,22 @@ numInStr(["this is a test", "test1"]) ➞ ["test1"]
 Notas
 As strings podem conter espaços em branco ou qualquer tipo de caractere.
 Bônus: tente resolver isso sem RegEx. */
+
+//1-filtrar as string com numeros
+//2-string sem numeros retorno um array vazio
+
+function numInStr(arr) {
+  let contemNumber = [];
+  for (let str of arr) {
+    for (let num of str) {
+      if (Number(num)) {
+        contemNumber.push(str);
+      }
+    }
+  }
+
+  return contemNumber;
+}
+console.log(numInStr(['1a', 'a', '2b', 'b']));
+console.log(numInStr(['abc', 'acb10']));
+console.log(numInStr(['this is a test', 'test1']));
